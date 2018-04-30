121e0fe14128744fc5addb2cab772c608fc04308
Pinger <wanpingping@a8sport.com>
2018-04-30 20:55:14 +0800

    update social component md

---
+++ sample/src/main/java/com/fungo/repertory/social/SocialActivity.kt
@@ -0,0 +1,20 @@
+package com.fungo.repertory.social
+
+import com.fungo.baselib.base.basic.BaseActivity
+import com.fungo.repertory.R
+
+/**
+ * @author Pinger
+ * @since 2018/4/30 20:48
+ * 社会化组件
+ */
+class SocialActivity : BaseActivity() {
+
+    override val layoutResID: Int
+        get() = R.layout.activity_social
+
+    override fun initView() {
+
+    }
+
+}
