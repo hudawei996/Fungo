5be42a16ff8226b8d8b5b9417fba53db96c4c7b2
Pinger <wanitme@gmail.com>
2018-07-02 14:47:55 +0800

    update weibo app key.

--- sample/src/main/java/com/fungo/repertory/constant/AppConstant.kt
+++ sample/src/main/java/com/fungo/repertory/constant/AppConstant.kt
@@ -10,4 +10,7 @@ object AppConstant {
     const val QQ_APP_ID = "1106936073"
     const val QQ_APP_IKEY = "5vG9ZTmAtVoOOxDk"
 
+    const val SINA_APP_KEY = "1840611664"
+    const val SINA_APP_SECRET = "986cb2dfc187a273360f533bf1a608e1"
+
 }
